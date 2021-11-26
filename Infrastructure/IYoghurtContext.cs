717754c22bada611aa2976d086f9e1117e380b3a
kaeppen <seho@itu.dk>
2021-11-26 15:36:44 +0100

    start filling in db context

--- Infrastructure/IYoghurtContext.cs
+++ Infrastructure/IYoghurtContext.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using YoghurtBank.Data.Model;
+
 namespace YoghurtBank.Infrastructure
 {
     public interface IYoghurtContext : IDisposable
     {
+        public DbSet<User> Users {get;}
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
         
     }
 }
--- Infrastructure/YoghurtContext.cs
+++ Infrastructure/YoghurtContext.cs
@@ -1,11 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using YoghurtBank.Data.Model;
 
 namespace YoghurtBank.Infrastructure
 {
     public class YoghurtContext : DbContext, IYoghurtContext
     {
         public YoghurtContext(DbContextOptions<YoghurtContext> options) : base(options) { }
-
+        public DbSet<User> Users {get; set;}
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
         
