6eeebe2001abea69cb5891eb207b6958fd17a66e
kaeppen <seho@itu.dk>
2021-11-26 15:36:53 +0100

    fiddle around with accessing db

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using YoghurtBank.Data;
+using YoghurtBank.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using YoghurtBank.Data.Model;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+//database connection
+builder.Services.AddDbContextFactory<YoghurtContext>(opt => opt.UseNpgsql(@"Host=localhost;Database=entitycore;Username=postgres;Password=mypassword"));
+builder.Services.AddScoped<IYoghurtContext, YoghurtContext>();
+
 
 var app = builder.Build();
 
@@ -29,3 +38,11 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+var context = app.Services.GetService<IYoghurtContext>();
+var usertest = new Student{Id = 1};
+context.Users.Add(usertest);
+context.SaveChangesAsync();
+
+
+
