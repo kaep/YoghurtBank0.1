30f11cfe12bd02aab2dc86a0111672af74782f01
kaeppen <seho@itu.dk>
2021-12-01 15:24:53 +0100

    Add dbsets

--- Infrastructure/IYoghurtContext.cs
+++ Infrastructure/IYoghurtContext.cs
@@ -6,6 +6,8 @@ namespace YoghurtBank.Infrastructure
     public interface IYoghurtContext : IDisposable
     {
         public DbSet<User> Users {get;}
+        public DbSet<Idea> Ideas { get; }
+        public DbSet<CollaborationRequest> CollaborationRequests { get; set; }
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
         
