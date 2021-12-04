7774f4362c7f4d02acccda532ee72af57752cdf7
kaeppen <74770239+kaeppen@users.noreply.github.com>
2021-12-04 14:45:18 +0100

    Implement and test most of CollaborationRequestRepository (#4)
    * Create constuctor
    
    * Start creating tests
    
    * add sqllite to be used in testing
    
    * Change name
    
    * Start implementing methods (mostly housekeeping)
    
    * Add using statements (necessary in rider)
    
    * Start testing/implementing methods
    
    * CollabRequest Tests
    
    * Make tests green - needs a lot of cleanup atm!
    
    * Implement and test DeleteAsync + cleanup
    
    * Test and implement first iteration of updateAsync method
    
    * ??
    
    * typo
    
    Co-authored-by: Stefan <lanc@itu.dk>

--- Services/ICollaborationRequestRepository.cs
+++ Services/ICollaborationRequestRepository.cs
@@ -1,16 +1,22 @@
+using System.Collections.Generic;
 using System.Net;
+using System.Threading.Tasks;
 using YoghurtBank.Data.Model;
 
 namespace YoghurtBank.Services
 {
     public interface ICollaborationRequestRepository
     {
-        (HttpStatusCode, Task<UserDetailsDTO>) AddCollaboratinRequestAsync(CollaborationRequestCreateDTO requestCreateDTO);
+
+        Task<CollaborationRequestDetailsDTO> CreateAsync(CollaborationRequestCreateDTO request);
+        Task<CollaborationRequestDetailsDTO> FindById(int id); //aka GET
+        Task<int> DeleteAsync(int id); //returværdi skal overvejes -> det skal nok være noget status-agtigt
+
+        Task<IEnumerable<CollaborationRequestDetailsDTO>> FindRequestsByIdeaAsync(int ideaId);
+
+        Task<CollaborationRequestDetailsDTO> UpdateAsync(int id, CollaborationRequestUpdateDTO updateRequest); //return value? like delete
         
-
-        (HttpStatusCode, Task<IEnumerable<CollaborationRequestDetailsDTO>>) FindRequestsByIdeaAsync(int ideaId);
-      
-
+        
         (HttpStatusCode, Task<IEnumerable<CollaborationRequestDetailsDTO>>) FindRequestsByUserAsync(int userId);
     }
 }
